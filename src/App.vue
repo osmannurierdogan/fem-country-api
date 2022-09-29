b598faa1e5cfc684143022acad6d85d569e9dfd4
Osman Nuri ERDOGAN <osmerd04@gmail.com>
2022-09-30 01:59:57 +0300

    Structure updated. country.capital[0] throws error

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,57 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+/* import * as dotenv from "dotenv";
+dotenv.config(); */
+//const BASE_URL = process.env.BASE_URL || "https://restcountries.com/v3.1/all";
+import { onMounted, ref } from "vue";
+import CountryList from "@/components/CountryList.vue";
+import SearchCountry from "@/components/SearchCountry.vue";
+import FilterList from "./components/FilterList.vue";
+const BASE_URL = "https://restcountries.com/v3.1";
+const countryList = ref([]);
+const fetchData = async () => {
+  await fetch(`${BASE_URL}/all`)
+    .then((response) => response.json())
+    .then((data) => {
+      countryList.value = data;
+      console.log("data :>> ", data);
+      console.log("countryList :>> ", countryList.value);
+    });
+};
+onMounted(() => {
+  fetchData();
+});
 </script>
-
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<template lang="pug">
+main.container
+  div.app
+    h3 Osman
+    h3 {{ BASE_URL }}
+    SearchCountry
+    FilterList
+    CountryList
+    div.flex
+      div.item(v-for="country in countryList" :key="country")
+        pre {{ country }}
+        h5 {{ country.name.common }}
+        h5 {{ country.population }}
+        h5 {{ country.region }}
+        pre {{ country.capital || "="}}
+        img(:src="country.flags.svg", width="30")
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+.flex {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+  gap: 2rem;
+  flex-wrap: wrap;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.item {
+  border: 1px solid red;
 }
 </style>
+
---
+++ src/components/SearchCountry.vue
@@ -0,0 +1,3 @@
+<template lang="pug">
+h3 Search Country
+</template>
